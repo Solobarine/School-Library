da17ffc248876482deeb89326cea75f5576fd667
solly <solobarine@gmail.com>
2022-12-21 16:39:06 +0100

    Create Student Class

---
+++ Student.rb
@@ -0,0 +1,10 @@
+class Student < Person
+  def initialize(name, age, parent_permission, classroom)
+    @classroom = classroom
+    super(name, age)
+  end
+
+  def play_hooky
+    return '¯\(ツ)/¯'
+  end
+end
